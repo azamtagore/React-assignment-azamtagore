c50a9563ea8ce9e273306e7715959eda4b9c2a86
Azam Yousuf Tagore <azamtagore@gmail.com>
2026-09-25 18:26:13 +0600

    Navbar and Hero Section design updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,20 +2,20 @@ const Hero = () => {
     return (
         <div className="container mx-auto flex justify-between justify-content text-2xl items-center p-4 bg-gray-100 text-black">
             <section>
-                <h1 className="text-4xl font-bold text-black-900"> Build Your Ideal</h1>
-                <h1 className="text-4xl font-bold textbrand-gradient"> Development Stack</h1>
-                <p> Explore frontend, backend, database, and tooling options,
+                <h1 className="text-6xl font-bold text-black-900"> Build Your Ideal</h1>
+                <h1 className="brand-gradient text-6xl font-bold textbrand-gradient"> Development Stack</h1>
+                <p className="p-10"> Explore frontend, backend, database, and tooling options,
                     compare them side by side, and put together the stack that fits your
                     next project.</p>
                 <div className="space-x-4">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-brand-gradient hover:bg-blue-700 text-white font-bold p-4 rounded-2xl">
                         Explore Technologies
                     </button>
-                    <button> Learn More</button>
+                    <button className="border border-gray-400 m-2 font-bold p-4 rounded-2xl"> Learn More</button>
                 </div>
             </section>
             <section>
-                <img src="/src/assets/banner-stack.png" alt="hero" />
+                <img className= "w-200" src="/src/assets/banner-stack.png" alt="hero" />
             </section>
         </div>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <nav  className="container mx-auto flex justify-between justify-content text-2xl items-center p-4 bg-gray-100 text-black">
+        <nav  className="container mx-auto flex justify-between justify-content content-center text-2xl items-center p-4 bg-gray-100 text-black">
 <div><img className="h-10" src="/src/assets/logo-text.png" alt="logo" /></div>
 <div>
 <ul className="flex space-x-4">
@@ -13,7 +13,7 @@ const Navbar = () => {
 </div>
 <div className="flex space-x-4">
     <p>Sign in</p>
-<button className="btn btn-active btn-secondary">Sign UP</button>
+<button className="btn btn-active btn-secondary h-12 text-xl bold rounded-2xl px-6 py-3">Sign UP</button>
     </div>
 
         </nav>
